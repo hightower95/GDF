a74cb43b49ac5863935721da709913a5475320f0
Hightower <HightowerArma3@gmail.com>
2018-04-10 18:15:51 +0100

    sketching ideas out

---
+++ loadout/common.hpp
@@ -0,0 +1,20 @@
+
+#define 
+
+
+_army AddBaseRole("Autorifleman",
+    helmet="helmet_classname",
+    ChestRig="blah",
+
+role.helmet = 
+role.helmet_alternate = 
+role.weapon.primary =
+role.weapon.primary.magazines = 
+role.weapon.primary.magazines_alternate =
+role.weapon_alternates
+
+#define role.weapon select 0
+#define autorifleman select 0
+#define .helmet select 0
+
+[["Autorifleman", ["Helmet","Helmet_ClassName"],["ChestRig",["Blah","Blah2"]],["Rifleman", []]
