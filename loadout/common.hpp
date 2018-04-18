9cc571ca8115fec6003943c2535ff4a0ced9ab7c
Hightower <HightowerArma3@gmail.com>
2018-04-18 11:54:22 +0100

    some thoughts are captured here

--- loadout/common.hpp
+++ loadout/common.hpp
@@ -13,8 +13,162 @@ role.weapon.primary.magazines =
 role.weapon.primary.magazines_alternate =
 role.weapon_alternates
 
-#define role.weapon select 0
-#define autorifleman select 0
-#define .helmet select 0
+#define options []
+#define loadout_item ["", options]
 
-[["Autorifleman", ["Helmet","Helmet_ClassName"],["ChestRig",["Blah","Blah2"]],["Rifleman", []]
+#define helmet loadout_item('helmet')
+#define empty_loadout [helmet, chest_rig, weapon ]
+//https://github.com/acemod/ACE3/blob/master/tools/cba/addons/main/script_macros_common.hpp
+
+#define HELMET 1
+#define CHEST 2
+
+
+#define STD_LDOUT NAME + HELMET + RIG + BACKPACK + UNIFORM + WEAPON_PRIM + WEAPON_SEC + WEAPON_PISTOL + ITEMS
+#define CREATE_LOADOUT(name) [name];
+
+
+#define SET_ITEM(loadout, index, classname) loadout set [index, classname]
+#define SET_ITEM_OPTIONS(loadout, index, options) loadout set[(index + 1), options]
+
+#define GET_ITEM(loadout, index) loadout select index
+#define GET_ITEM(loadout, index) loadout select (index+1)
+
+#define SET_HELMET(loadout, classname) SET_ITEM(loadout,HELMET,classname) 
+#define GET_HELMET(loadout) GET_ITEM(loadout,HELMET)
+#define ASSIGN_HELMET(loadout, unit) (unit removeHeadgear; unit assignHeadgear (GET_HELMET(loadout))) 
+
+// 
+Default_Loadout
+
+set_default_rifle
+-> loadouts
+  -- functions
+  -- cfgs 
+  -> blufor
+    -- base
+    -> faction
+      "register_faction"
+      "get_loadout_callback" 
+    -> faction
+  -> opfor
+  -> ind
+
+class loadout_roles {
+    class base {
+        name = "rifleman";
+        modifiers[] = [];
+        skills[] = [];
+    };
+    class autorifleman : base {
+        name = "autorifle";
+        modifiers[] = [];
+        skills = ["unsteady", "strong"];
+        weapon = automatic_light;
+        optics = optics_close;
+        
+    };
+};
+
+["rifleman", "us", ["light", "sl"]] call f_fnc_assignGear
+
+case "rifleman":
+    player addWeapon WEAPON_RIFLEMAN
+    player addMagazines [PRIMARY_MAGS, 10]
+
+if count (modifiers select {_x == "SL"}) > 0 then {
+    player addItem "item_gps";
+    player addItem "binoculars";
+}
+
+python:
+{
+"rifleman":{
+    "uniform": {
+        "class": "cls_name_uni",
+        "contents" : ["this", "that"]
+    },
+    "chest": {
+        "default": "cls_name_uni",
+        "options": ""
+        "contents" : ["this", "that"]
+    }
+}
+}
+
+roles = {
+    "rifleman": {
+        article : article type, default, options
+        "weapon_primary" : {
+            "weapon_default" : {
+                "class": "clsname",
+                "magazines": [("tracer", 5), ("std", 7)],
+                "optic": {
+                    "default": "blah",
+                    "alternates": [
+                        {
+                        "cls": "clsname",
+                        "test": "isNight"
+                        }
+                    ]
+                }
+            },
+            "alternates": [
+                 {
+                    "class": "clsname",
+                    "magazines": [("tracer", 5), ("std", 7)]
+                },
+                 {
+                    "class": "clsname",
+                    "magazines": [("tracer", 5), ("std", 7)]
+                },
+            ],
+            "optics": 
+        }
+    }
+
+}
+
+roles.hpp
+case rifleman:
+[{magazines},{grenades},{smokes},{gps},{medic_supplies}]
+{magazines} = {{primary_mags}{secondary_mags}{tetiary_mags}}
+{primary_mags} = {{normal}{tracer}}
+
+nato.sqf:
+{normal_mags} : "556stdx5v_clsname"
+
+["{magazines}"]
+
+"{magazines}" : ["{primary_mags}",
+
+
+
+
+//[["Autorifleman", ["Helmet","Helmet_ClassName"],["ChestRig",["Blah","Blah2"]],["Rifleman", []]
+
+_unit = player;
+
+role = getRole (_unit)
+           -> guessByVariable (_unit getVariable "role")
+           -> guessByClassName (className _unit)
+           -> guessByItems (if medikit, if toolkit, if explosives)
+           -> guessByWeapon (if magazine bullets > 10 > 30, has GL, has AT)
+           -> otherTest
+faction = getFaction (_unit)
+faction_roles = getGear (faction)
+
+loadout = lookup_loadout(role)
+
+faction = ["NewArmy"] call addFaction;
+faction setDefault 
+faction addRole (rolename, role class names, items, weapon, test)
+faction setEquipment
+
+faction = [roles, role_tests, faction_name]
+roles = STD_ROLES
+roles removeRole ("rifleman")
+
+there has to be elements: [palette of roles, palette of gear]
+the role maps gear to a faction
+the gear palette maps faction classname to items
---
+++ loadout/example.sqf
@@ -0,0 +1,119 @@
+
+// getUnitLoadout = 
+[
+    [
+        "arifle_MX_SW_pointer_F", // rifle
+        "", // silencer
+        "acc_pointer_IR", // laser
+        "", // optic
+        [
+            "100Rnd_65x39_caseless_mag", // loaded magazine
+            0 // rounds left 
+        ],
+        [], // secondary magazine?
+        "bipod_01_F_snd" // bipod
+    ], // primary
+    [], // launcher
+    [
+        "hgun_P07_F",
+        "",
+        "",
+        "",
+        ["16Rnd_9x21_Mag",17],
+        [],
+        ""
+    ],
+    [
+        "U_B_CombatUniform_mcam_tshirt",
+        [
+            ["ACE_EarPlugs",1],
+            ["ACE_fieldDressing",2],
+            ["ACE_morphine",1],
+            ["SmokeShell",1,1],
+            ["HandGrenade",1,1],
+            ["SmokeShellGreen",1,1],
+            ["Chemlight_green",1,1]
+        ]
+    ],
+    [
+        "V_PlateCarrier2_rgr",
+        [
+            ["100Rnd_65x39_caseless_mag",5,100],
+            ["16Rnd_9x21_Mag",2,17],
+            ["Chemlight_green",1,1]
+        ]
+    ],
+    [], // backpack
+    "H_HelmetB_grass", //helmet
+    "", //glasse?
+    [], //binoculars
+    ["ItemMap","","tf_anprc152_1","ItemCompass","tf_microdagr","NVGoggles"] //map, gps, radio, compass, watch, nvgs
+]
+
+// an option step before:
+[
+    [
+		[
+        "arifle_MX_SW_pointer_F", // rifle
+        "", // silencer
+        ["default", ["acc_pointer_IR","something"]], // laser options
+        ["default", ["blah", "whatever"]], // optic
+        [
+            "100Rnd_65x39_caseless_mag", // loaded magazine
+            0 // rounds left 
+        ],
+        [], // secondary magazine?
+        "bipod_01_F_snd", // bipod
+		"magazine_main" // main mag
+		"magazine_tracer" // tracer
+		// I think GLs dont need alternates - because its standard loadouts.
+    	]
+	], // primary
+    [], // launcher
+    [
+        "hgun_P07_F",
+        "",
+        "",
+        "",
+        ["16Rnd_9x21_Mag",17],
+        [],
+        ""
+    ],
+    [
+        "U_B_CombatUniform_mcam_tshirt",
+        [
+            ["ACE_EarPlugs",1],
+            ["ACE_fieldDressing",2],
+            ["ACE_morphine",1],
+            ["SmokeShell",1,1],
+            ["HandGrenade",1,1],
+            ["SmokeShellGreen",1,1],
+            ["Chemlight_green",1,1]
+        ]
+    ],
+    [
+        
+		[
+			"V_PlateCarrier2_rgr",
+			[
+				["100Rnd_65x39_caseless_mag",5,100],
+				["16Rnd_9x21_Mag",2,17],
+				["Chemlight_green",1,1]
+			]
+		],
+		[
+			["rig_lite", "night"], ["rig_lite_other", "day"]
+		]
+		
+    ],
+    [], // backpack
+    "H_HelmetB_grass", //helmet
+    "", //glasse?
+    [], //binoculars
+    ["ItemMap","","tf_anprc152_1","ItemCompass","tf_microdagr","NVGoggles"] //map, gps, radio, compass, watch, nvgs
+]
+
+role -> map(item_names to faction_classes) -> palette -> global filters -> final loadout
+
+
+
